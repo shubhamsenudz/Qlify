8d678cd82cd3d8f88e228963738f6ff8d120adf9
Shubham Sen <shubhamsen.udz@gmail.com>
2024-04-02 16:24:51 +0530

    mes22353

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Logo.module.css";
-import LogoImg from "../../assets/logo.png";
+import LogoImg from "../../assets/BrandLogo.png";
 
 const Logo=()=>{
 

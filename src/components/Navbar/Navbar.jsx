01a3db4b631b8cde5d2f5f714d77a32fe6d9fdd9
Shubham Sen <shubhamsen.udz@gmail.com>
2024-04-02 16:23:48 +0530

    mes22343

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Navbar.module.css";
 import Logo from "../Logo/Logo";
-import SearchBar from "../SearchBar/SearchBar";
+import SearchBar from "../Search/Search";
 import Button from "../Button/Button";
 
 const Navbar = () => {

b265e49086d28bef7f749f9d8801f5c6ffe1f8a5
Shubham Sen <shubhamsen.udz@gmail.com>
2024-04-02 17:06:08 +0530

    mes2212111

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,13 +5,15 @@ import SearchBar from "../Search/Search";
 import Button from "../Button/Button";
 
 const Navbar = () => {
-    return (
+  
+      return (
         <div className={styles.navbar}>
-            <Logo />
-            <SearchBar search={"Search a song of your choice"} />
-            <Button children={"Give Feedback"} />
+            <Logo data-test="logo" />
+            <SearchBar data-test="searchBar" search={"Search a song of your choice"} />
+            <Button data-test="feedbackButton" children={"Give Feedback"} />
         </div>
     );
+    
 }
 
 export default Navbar;
